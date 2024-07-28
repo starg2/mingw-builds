38f3b9cc25e7d47584830138a238b84a31741fec
starg2 <75976488+starg2@users.noreply.github.com>
2024-07-28 23:15:50 +0900

    Update mingw-w64

--- scripts/mingw-w64-download.sh
+++ scripts/mingw-w64-download.sh
@@ -51,7 +51,7 @@ PKG_NAME=mingw-w64-${RUNTIME_VERSION}
 	)
 	[[ $RUNTIME_BRANCH == master ]] && {
 		PKG_EXECUTE_AFTER_UNCOMPRESS=(
-			"git reset --hard 561a057cebee2824f0cea44ebc785719ca873337" # Reset to this commit hash for reproducible builds
+			"git reset --hard f785aaee1781a4fbb3322ed44375208c24e2cf21" # Reset to this commit hash for reproducible builds
 		)
 	}
 }
