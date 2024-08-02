c972b67a1e31b3239cd6648d72e86dd1a7053b74
starg2 <75976488+starg2@users.noreply.github.com>
2024-08-03 06:34:21 +0900

    Update gdb to 15.1

--- scripts/gdb.sh
+++ scripts/gdb.sh
@@ -35,11 +35,11 @@
 
 # **************************************************************************
 
-PKG_VERSION=14.2
+PKG_VERSION=15.1
 PKG_NAME=gdb-${PKG_VERSION}
 PKG_DIR_NAME=gdb-${PKG_VERSION}
 PKG_TYPE=.tar.xz
-PKG_SHA256=2d4dd8061d8ded12b6c63f55e45344881e8226105f4d2a9b234040efa5ce7772
+PKG_SHA256=38254eacd4572134bca9c5a5aa4d4ca564cbbd30c369d881f733fb6b903354f2
 PKG_URLS=(
 	"https://ftpmirror.gnu.org/gnu/gdb/gdb-${PKG_VERSION}${PKG_TYPE}|sha256:${PKG_SHA256}"
 )
