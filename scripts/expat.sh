3e086348138d574be7db56ed0851c5e921ac89a6
starg2 <starg2@envs.net>
2025-09-29 19:22:58 +0900

    Upgrade expat to 2.7.3

--- scripts/expat.sh
+++ scripts/expat.sh
@@ -35,11 +35,11 @@
 
 # **************************************************************************
 
-PKG_VERSION=2.7.1
+PKG_VERSION=2.7.3
 PKG_NAME=expat-${PKG_VERSION}
 PKG_DIR_NAME=expat-${PKG_VERSION}
 PKG_TYPE=.tar.xz
-PKG_SHA256=354552544b8f99012e5062f7d570ec77f14b412a3ff5c7d8d0dae62c0d217c30
+PKG_SHA256=71df8f40706a7bb0a80a5367079ea75d91da4f8c65c58ec59bcdfbf7decdab9f
 PKG_URLS=(
 	"https://github.com/libexpat/libexpat/releases/download/R_${PKG_VERSION//./_}/expat-${PKG_VERSION}${PKG_TYPE}|sha256:${PKG_SHA256}"
 )
