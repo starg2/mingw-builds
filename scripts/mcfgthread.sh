a502d63214e854f8f40634ed644cb4811059d385
starg2 <starg2@envs.net>
2026-03-30 19:03:44 +0900

    Upgrade mcfgthread to v2.3-ga.2

--- scripts/mcfgthread.sh
+++ scripts/mcfgthread.sh
@@ -48,7 +48,7 @@ PKG_PRIORITY=prereq
 #
 
 PKG_EXECUTE_AFTER_UNCOMPRESS=(
-	"git reset --hard 3fb7e52e52bc59e4c4810e6348979f79afb86ddd" # Reset to this commit hash for reproducible builds
+	"git reset --hard 3662be1e1c0ce6aa5772526f439d786b50e67298" # Reset to this commit hash for reproducible builds
 )
 
 #
