64ba75013b20902be57bb9e4fed8fee30c450aab
starg2 <starg2@envs.net>
2026-05-18 18:10:14 +0900

    gcc-16.1.0: Backport patch "libstdc++: Make configure check for atomics work on Windows [PR125312]"

--- scripts/gcc-16.1.0.sh
+++ scripts/gcc-16.1.0.sh
@@ -60,6 +60,7 @@ PKG_PATCHES=(
 	gcc/gcc-10-libgcc-ldflags.patch
 	gcc/gcc-12-replace-abort-with-fancy_abort.patch
 	gcc/gcc-13-mcf-sjlj-avoid-infinite-recursion.patch
+	gcc/gcc-16.1.0-libstdcxx-atomic-check.patch
 )
 
 #
